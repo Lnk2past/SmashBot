1c9167edf05697d921af02dc13ca9be06ea4e741
Lnk2past <6456398+Lnk2past@users.noreply.github.com>
2019-04-13 16:27:25 +0000

    Fix total matchup play count

--- commands/report.js
+++ commands/report.js
@@ -1,10 +1,12 @@
 var PLAYERS = 'SELECT * FROM player';
 var CHARACTERS = 'SELECT * FROM character';
 var MAIN_PLAYER = 'SELECT player_id,display_name FROM player WHERE player_name=$1';
+var MAIN_CHARACTER = 'SELECT * FROM chaaracter WHERE character_name=$1';
 var GAMES_PLAYED_BY_PLAYER = 'SELECT game.game_id FROM game LEFT JOIN pcg ON pcg.game_id=game.game_id WHERE pcg.player_id=$1 ORDER BY game.game_id';
 var PCG_FROM_GAME_ORDERED_BY_PLAYER = 'SELECT * FROM pcg WHERE game_id=$1 ORDER BY CASE player_id WHEN $2 THEN 1 ELSE 2 END';
 var PCG_FROM_GAME = 'SELECT * from pcg WHERE game_id=$1';
 var PCG_PLAYED_BY_PLAYER = 'SELECT * FROM pcg WHERE player_id=$1';
+var PCG_PLAYED_BY_PLAYER_AS_CHAR = 'SELECT COUNT(*) FROM pcg WHERE player_id=$1 AND character_id=$2';
 var PCG_WON_BY_PLAYER = 'SELECT COUNT(*) FROM pcg WHERE player_id=$1 and win=true';
 var MATCHES_PLAYED_BY_PLAYER = 'SELECT COUNT(DISTINCT match.match_id) FROM match LEFT JOIN game ON game.match_id=match.match_id LEFT JOIN pcg ON pcg.game_id=game.game_id WHERE pcg.player_id=$1';
 var MATCHES_WON_BY_PLAYER = 'SELECT COUNT(*) FROM match WHERE winner=$1';
@@ -98,6 +100,11 @@ report_player: async function(pool, discord_msg, content) {
     var report = '```' + generateOverallReport(player_display_name, matches_played, matches_won, pcg_played, pcg_won, player_char_data) + '```'
     if (character != '') {
         if (character in char_matchups) {
+            var character_res = await pool.query(MAIN_CHARACTER, [character]);
+            var character_id = character_res.rows[0].character_id;
+            var pcg_played_as_char_res = await pool.query(PCG_PLAYED_BY_PLAYER_AS_CHAR, [player_id, character_id]);
+            var pcg_player_as_char = pcg_played_as_char_res.rows[0].count;
+
             char_matchup = char_matchups[character];
             var char_matchup_data = Object.keys(char_matchup).map(function(key) {
                 return [key, char_matchup[key]];
@@ -108,7 +115,7 @@ report_player: async function(pool, discord_msg, content) {
                 var wp2 = second[1][0];
                 return wp2 - wp1;
             });    
-            report += '```' + generateMatchupReport(character, char_matchup_data, pcg_played) + '```'
+            report += '```' + generateMatchupReport(character, char_matchup_data, pcg_player_as_char) + '```'
         }
         else {
             report += '```Could not find any matchup data for player ' + player_display_name + ' and character ' + character + '```';
@@ -162,14 +169,14 @@ function generateOverallReport(player_display_name, matches_played, matches_won,
     return report;
 }
 
-function generateMatchupReport(character, char_matchup_data, pcg_played) {
+function generateMatchupReport(character, char_matchup_data, pcg_player_as_char) {
     report = '';
     report += 'Matchup Summary: ' + character + '\n';
     for (var char_idx = 0; char_idx < char_matchup_data.length; char_idx++) {
         var p = char_matchup_data[char_idx][1][0];
         var w = char_matchup_data[char_idx][1][1];
         var char_str = (char_matchup_data[char_idx][0] + ':').padEnd(20);
-        report += '   ' + char_str + getCharacterRecordString(pcg_played, p, w);
+        report += '   ' + char_str + getCharacterRecordString(pcg_player_as_char, p, w);
     }
     return report;}
 
