4281fe01a9b7b3c7aeb32f5657a3a849cc9ae531
Lnk2past <6456398+Lnk2past@users.noreply.github.com>
2019-06-05 02:58:45 +0000

    Move queries to module

---
+++ commands/bracket.js
@@ -0,0 +1,14 @@
+module.exports = 
+{
+    create_season: function() {
+
+    },
+
+    end_season: function() {
+
+    },
+
+    create_matches: function() {
+
+    }
+}
--- commands/report.js
+++ commands/report.js
@@ -1,38 +1,23 @@
-var PLAYERS = 'SELECT * FROM player';
-var CHARACTERS = 'SELECT * FROM character';
-var MAIN_PLAYER = 'SELECT player_id,display_name FROM player WHERE player_name=$1';
-var MAIN_CHARACTER = 'SELECT * FROM character WHERE character_name=$1';
-var GAMES_PLAYED_BY_PLAYER = 'SELECT game.game_id FROM game LEFT JOIN pcg ON pcg.game_id=game.game_id WHERE pcg.player_id=$1 ORDER BY game.game_id';
-var PCG_FROM_GAME_ORDERED_BY_PLAYER = 'SELECT * FROM pcg WHERE game_id=$1 ORDER BY CASE player_id WHEN $2 THEN 1 ELSE 2 END';
-var PCG_FROM_GAME = 'SELECT * from pcg WHERE game_id=$1';
-var PCG_PLAYED_BY_PLAYER = 'SELECT * FROM pcg WHERE player_id=$1';
-var PCG_PLAYED_BY_PLAYER_AS_CHAR = 'SELECT COUNT(*) FROM pcg WHERE player_id=$1 AND character_id=$2';
-var PCG_WON_BY_PLAYER = 'SELECT COUNT(*) FROM pcg WHERE player_id=$1 and win=true';
-var MATCHES_PLAYED_BY_PLAYER = 'SELECT COUNT(DISTINCT match.match_id) FROM match LEFT JOIN game ON game.match_id=match.match_id LEFT JOIN pcg ON pcg.game_id=game.game_id WHERE pcg.player_id=$1';
-var MATCHES_WON_BY_PLAYER = 'SELECT COUNT(*) FROM match WHERE winner=$1';
+dbqueries = require('../dbqueries');
 
 module.exports = 
 {
 report_player: async function(pool, discord_msg, content) {
     var [player,character] = parseArguments(content, discord_msg.author.username);
-    var [player_id, player_display_name] = await getPlayerInfo(pool, player);
+    var [player_id, player_display_name] = await dbqueries.get_primary_player(pool, player);
 
     var players = await mapPlayerNames(pool);
     var chars = await mapCharacterNames(pool);
 
-    var games_played_res = await pool.query(GAMES_PLAYED_BY_PLAYER, [player_id]);
-    var pcg_played_res = await pool.query(PCG_PLAYED_BY_PLAYER, [player_id]);
-    var pcg_won_res = await pool.query(PCG_WON_BY_PLAYER, [player_id]);
-    var matches_played_res = await pool.query(MATCHES_PLAYED_BY_PLAYER, [player_id]);
-    var matches_won_res = await pool.query(MATCHES_WON_BY_PLAYER, [player_id]);
-
-    var pcg_played = pcg_played_res.rows.length;
-    var pcg_won = pcg_won_res.rows[0].count;
-    var matches_played = matches_played_res.rows[0].count;
-    var matches_won = matches_won_res.rows[0].count;
+    var games_played = await dbqueries.get_games_played_by_player(pool, player_id);
+    var pcgs_played = await dbqueries.get_pcgs_played_by_player(pool, player_id);
+    var pcg_play_count = pcgs_played.length;
+    var pcg_win_count = await dbqueries.get_pcgs_won_by_player(pool, player_id);
+    var match_play_count = await dbqueries.get_matches_played_by_player(pool, player_id);
+    var matches_win_count = await dbqueries.get_matches_won_by_player(pool, player_id);
 
     var chars_played = {};
-    await asyncForEach(pcg_played_res.rows, async (pcg) => {
+    await asyncForEach(pcgs_played, async (pcg) => {
         var char = chars[pcg.character_id];
         if (!(char in chars_played)) {
             chars_played[char] = [0,0];
@@ -54,11 +39,8 @@ report_player: async function(pool, discord_msg, content) {
     });
 
     var char_matchups = {};
-    await asyncForEach(games_played_res.rows, async (game) => {
-        pcg_from_game_res = await pool.query(PCG_FROM_GAME_ORDERED_BY_PLAYER, [game.game_id, player_id]);
-        player_pcg = pcg_from_game_res.rows[0];
-        opponent_pcg = pcg_from_game_res.rows[1];
-
+    await asyncForEach(games_played, async (game) => {
+        var [player_pcg, opponent_pcg] = await dbqueries.get_pcgs_from_games_ordered(pool, game.game_id, player_id);
         var char = chars[player_pcg.character_id];
         var opponent = chars[opponent_pcg.character_id];
 
@@ -75,14 +57,11 @@ report_player: async function(pool, discord_msg, content) {
         }
     });
 
-    var report = '```' + generateOverallReport(player_display_name, matches_played, matches_won, pcg_played, pcg_won, player_char_data) + '```'
+    var report = '```' + generateOverallReport(player_display_name, match_play_count, matches_win_count, pcg_play_count, pcg_win_count, player_char_data) + '```'
     if (character != '') {
         if (character in char_matchups) {
-            var character_res = await pool.query(MAIN_CHARACTER, [character]);
-            var character_id = character_res.rows[0].character_id;
-            var pcg_played_as_char_res = await pool.query(PCG_PLAYED_BY_PLAYER_AS_CHAR, [player_id, character_id]);
-            var pcg_player_as_char = pcg_played_as_char_res.rows[0].count;
-
+            var character_id = await dbqueries.get_primary_character(pool, character);
+            var pcg_player_as_char = await dbqueries.get_pcgs_played_by_player_as_char(pool, player_id, character_id);
             char_matchup = char_matchups[character];
             var char_matchup_data = Object.keys(char_matchup).map(function(key) {
                 return [key, char_matchup[key]];
@@ -129,8 +108,8 @@ function parseArguments(content, message_username) {
 }
 
 async function mapPlayerNames(pool) {
-    var player_list_res = await pool.query(PLAYERS);
-    var players = player_list_res.rows.reduce(function(map, obj) {
+    var player_list = await dbqueries.get_players(pool);
+    var players = player_list.reduce(function(map, obj) {
         map[obj.player_id] = obj.player_name;
         return map;
     }, {});
@@ -138,20 +117,14 @@ async function mapPlayerNames(pool) {
 }
 
 async function mapCharacterNames(pool) {
-    var char_list_res = await pool.query(CHARACTERS);
-    var chars = char_list_res.rows.reduce(function(map, obj) {
+    var char_list = await dbqueries.get_characters(pool);
+    var chars = char_list.reduce(function(map, obj) {
         map[obj.character_id] = obj.character_name;
         return map;
     }, {});
     return chars;
 }
 
-async function getPlayerInfo(pool, player) {
-    var player_res = await pool.query(MAIN_PLAYER, [player]);
-    var player_id = player_res.rows[0].player_id;
-    var player_display_name = player_res.rows[0].display_name;
-    return [player_id, player_display_name];
-}
 
 function convertResultRowsToDict(result) {
     return result.rows.reduce(function(map, obj) {
@@ -175,17 +148,17 @@ function getCharacterRecordString(total_played, played, won) {
     return (won + '-' + lost).padEnd(8)  + ' ' + play_stats_str + win_stats_str + '\n';
 }
 
-function generateOverallReport(player_display_name, matches_played, matches_won, pcg_played, pcg_won, player_char_data) {
+function generateOverallReport(player_display_name, matches_played, matches_win_count, pcg_play_count, pcg_win_count, player_char_data) {
     report = '';
     report += 'Player Summary: ' + player_display_name + '\n';
-    report += 'Matches Record: ' + getRecordString(matches_played, matches_won);
-    report += 'Game Record:    ' + getRecordString(pcg_played, pcg_won);
+    report += 'Matches Record: ' + getRecordString(matches_played, matches_win_count);
+    report += 'Game Record:    ' + getRecordString(pcg_play_count, pcg_win_count);
     report += 'Top 5 Characters:\n'
     for (var char_idx = 0; char_idx < 5 && char_idx < player_char_data.length; char_idx++) {
         var p = player_char_data[char_idx][1][0];
         var w = player_char_data[char_idx][1][1];
         var char_str = (player_char_data[char_idx][0] + ':').padEnd(20);
-        report += '   ' + char_str + getCharacterRecordString(pcg_played, p, w);
+        report += '   ' + char_str + getCharacterRecordString(pcg_play_count, p, w);
     }
     return report;
 }
