610c32d22895a39269607870b6514dff7aaa028d
Lnk2past <6456398+Lnk2past@users.noreply.github.com>
2019-06-07 02:19:11 +0000

    Add debug statement

--- commands/bracket.js
+++ commands/bracket.js
@@ -5,15 +5,19 @@ module.exports =
     create_matches: async function(pool, discord_msg, content) {
         active_players = dbqueries.get_active_players(pool);
 
-        console.log('hello')
+        console.log('hello');
 
         for (player1 in active_players) {
             for (player2 in active_players) {
+                console.log(player1);
+                console.log(player2);
                 if (player1.player_id != player2.player_id) {
-                    console.log(player1.display_name + " - " + player2.display_name)
+                    console.log(player1.display_name + " - " + player2.display_name);
                 }
             }
         }
+
+        console.log('bye');
     }
 }
 
