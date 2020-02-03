ccf1493e2d31b358017f0857eca30ee0423da865
Lnk2past <Lnk2past@gmail.com>
2020-02-03 00:15:08 -0500

    Update user commands

--- commands/user.js
+++ commands/user.js
@@ -5,8 +5,8 @@ module.exports =
 register: async function(pool, discord_msg) {
     var discord_id = discord_msg.author.id;
     var discord_name = discord_msg.author.name;
-    player = await dbqueries.get_player_by_discord_id(pool, discord_id);
-    if (player.length === 0) {
+    [id, player] = await dbqueries.get_player_by_discord_id(pool, discord_id);
+    if (id === undefined) {
         await pool.query('INSERT INTO players(name, discord_id) VALUES($1, $2)', [discord_name, discord_id]);
     }
     else {
@@ -17,8 +17,8 @@ register: async function(pool, discord_msg) {
 update: async function(pool, discord_msg) {
     var discord_id = discord_msg.author.id;
     var discord_name = discord_msg.author.name;
-    player = await dbqueries.get_player_by_discord_id(pool, discord_id);
-    if (player.length === 0) {
+    [id, player] = await dbqueries.get_player_by_discord_id(pool, discord_id);
+    if (id === undefined) {
         discord_msg.channel.send('You are not registered yet!');
     }
     else {
